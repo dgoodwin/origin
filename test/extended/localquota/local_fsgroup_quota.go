299ba82e3dca032a74941c0c82966402da4936f2
Devan Goodwin <dgoodwin@redhat.com>
2016-03-10 08:25:52 -0400

    Cleanup actual tests.

--- test/extended/localquota/local_fsgroup_quota.go
+++ test/extended/localquota/local_fsgroup_quota.go
@@ -18,31 +18,18 @@ import (
 )
 
 const (
-	volumeDirVar       = "VOLUME_DIR"
-	projectName        = "local-quota"
-	podName            = "hello-openshift"
-	podCreationTimeout = 30 // seconds
-	expectedQuotaKb    = 262144
+	volDirEnvVar       = "VOLUME_DIR"
+	podCreationTimeout = 30     // seconds
+	expectedQuotaKb    = 262144 // launcher script sets 256Mi, xfs_quota reports in Kb.
 )
-
-func getEnvVar(key string) string {
-	for _, e := range os.Environ() {
-		pair := strings.Split(e, "=")
-		if pair[0] == key {
-			return pair[1]
-		}
-	}
-	return ""
-}
 
 // setFSGroupMustRunAs flips the restricted SCC's FSGroup setting
 // to MustRunAs. May not be required for long as this will soon be
-// the default.
+// the default with: https://github.com/openshift/origin/pull/7334
 func setFSGroupMustRunAs(oc *exutil.CLI) error {
 	// Write out to temp file so we can edit the SCC and update it:
 	tempFile, err := ioutil.TempFile("", "fsgroup-scc-edit")
 	defer os.Remove(tempFile.Name())
-	fmt.Printf("Created temp file: %s\n", tempFile.Name())
 
 	outBytes, err := oc.AsAdmin().Run("export").Args("scc/restricted").Output()
 	scc := string(outBytes)
@@ -57,18 +44,15 @@ func setFSGroupMustRunAs(oc *exutil.CLI) error {
 	if err != nil {
 		return err
 	}
-	fmt.Printf("Replace output: %s\n", string(outBytes))
 	return nil
 }
 
 func lookupFSGroup(oc *exutil.CLI, project string) (int, error) {
-	//oc get project/default --template='{{ index .metadata.annotations "openshift.io/sa.scc.supplemental-groups" }}'
-	gidRange, err := oc.Run("get").Args("project", project, "--template='{{ index .metadata.annotations \"openshift.io/sa.scc.supplemental-groups\" }}'").Output()
-	fmt.Println(err)
+	gidRange, err := oc.Run("get").Args("project", project,
+		"--template='{{ index .metadata.annotations \"openshift.io/sa.scc.supplemental-groups\" }}'").Output()
 	if err != nil {
 		return 0, err
 	}
-	fmt.Println(gidRange)
 
 	// gidRange will be something like: 1000030000/10000
 	fsGroupStr := strings.Split(gidRange, "/")[0]
@@ -82,17 +66,17 @@ func lookupFSGroup(oc *exutil.CLI, project string) (int, error) {
 	return fsGroup, nil
 }
 
-// Sample XFS quota report output:
-// $ xfs_quota -x -c 'report -n' /tmp/openshift
-// Group quota on /tmp/openshift (/dev/sdb2)
-//                               Blocks
-// Group ID         Used       Soft       Hard    Warn/Grace
-// ---------- --------------------------------------------------
-// #0              99004          0          0     00 [--------]
-// #1000          166916  268435456  268435456     00 [--------]
-
 // lookupXFSQuota runs an xfs_quota report and parses the output
 // looking for the given fsGroup ID's hard quota.
+//
+// Output from this command looks like:
+//
+// $ xfs_quota -x -c 'report -n  -L 1000030000 -U 1000030000' /tmp/openshift/xfs-vol-dir
+// Group quota on /tmp/openshift/xfs-vol-dir (/dev/sdb2)
+//                                Blocks
+// Group ID         Used       Soft       Hard    Warn/Grace
+// ---------- --------------------------------------------------
+// #1000030000          0     524288     524288     00 [--------]
 func lookupXFSQuota(oc *exutil.CLI, fsGroup int, volDir string) (int, error) {
 
 	// First lookup the filesystem device the volumeDir resides on:
@@ -104,12 +88,10 @@ func lookupXFSQuota(oc *exutil.CLI, fsGroup int, volDir string) (int, error) {
 	if parseErr != nil {
 		return 0, parseErr
 	}
-	fmt.Printf("Volume directory is on: %s\n", fsDevice)
 
 	args := []string{"xfs_quota", "-x", "-c",
 		fmt.Sprintf("report -n -L %d -U %d", fsGroup, fsGroup),
 		fsDevice}
-	fmt.Printf("%s\n", args)
 	cmd := exec.Command("sudo", args...)
 	var stderr bytes.Buffer
 	cmd.Stderr = &stderr
@@ -117,114 +99,89 @@ func lookupXFSQuota(oc *exutil.CLI, fsGroup int, volDir string) (int, error) {
 	if err != nil {
 		return 0, err
 	}
-	fmt.Printf("stderr: %s\n", stderr.String())
 	quotaReport := string(outBytes)
-	fmt.Printf("Got XFS Quota report: \n%s\n", quotaReport)
 
 	// Parse output looking for lines starting with a #, which are the lines with
 	// group IDs and their quotas:
 	lines := strings.Split(quotaReport, "\n")
-	var quotaFound int
+	var quota int
+	foundFsGroup := false
 	for _, l := range lines {
-		fmt.Printf("Got line: %s\n", l)
 		if strings.HasPrefix(l, fmt.Sprintf("#%d", fsGroup)) {
-			fmt.Println("Found!")
+			foundFsGroup = true
 			words := strings.Fields(l)
-			fmt.Printf("Words: %s\n", words)
 			if len(words) != 6 {
 				return 0, fmt.Errorf("expected 6 words in quota line: %s", l)
 			}
-			quotaFound, err := strconv.Atoi(words[3])
+			quota, err := strconv.Atoi(words[3])
 			if err != nil {
 				return 0, err
 			}
-			return quotaFound, nil
+			return quota, nil
 		}
 	}
-	if quotaFound == 0 {
-		return 0, fmt.Errorf("no quota found for group ID: %d", fsGroup)
+	if !foundFsGroup {
+		return 0, fmt.Errorf("unable to find any quota for group ID: %d", fsGroup)
 	}
 
-	return quotaFound, nil
+	return quota, nil
+}
+
+// waitForQuotaToBeApplied will check for the expected quota, and wait a short interval if
+// not found until we reach the timeout. If we were unable to find the quota we expected,
+// an error will be returned. If we found the expected quota in time we will return nil.
+func waitForQuotaToBeApplied(oc *exutil.CLI, fsGroup int, volDir string) error {
+	secondsWaited := 0
+	for secondsWaited < podCreationTimeout {
+		quotaFound, quotaErr := lookupXFSQuota(oc, fsGroup, volDir)
+		o.Expect(quotaErr).NotTo(o.HaveOccurred())
+		if quotaFound == expectedQuotaKb {
+			return nil
+		}
+
+		time.Sleep(1 * time.Second)
+		secondsWaited = secondsWaited + 1
+	}
+
+	return fmt.Errorf("expected quota was not applied in time")
 }
 
 var _ = g.Describe("[volumes] Test local storage quota", func() {
 	defer g.GinkgoRecover()
 	var (
-		oc                 = exutil.NewCLI(projectName, exutil.KubeConfigPath())
+		oc                 = exutil.NewCLI("local-quota", exutil.KubeConfigPath())
 		emptyDirPodFixture = exutil.FixturePath("..", "..", "examples", "hello-openshift", "hello-pod.json")
 	)
 
-	// TODO: Before we call this test, we need to modify node-config.yaml:
-	//
-	// volumeConfig:
-	//   localQuota:
-	//     fsGroup: 256Mi
-	//
-	// This may imply a new launcher script, there are a couple examples, but none seem to
-	// call golang test code...
 	g.Describe("FSGroup local storage quota", func() {
 		g.It("should be applied to XFS filesystem when a pod is created", func() {
-			fmt.Println("\n################ Running local storage quota tests")
-			fmt.Println(exutil.TestContext.OutputDir)
 			oc.SetOutputDir(exutil.TestContext.OutputDir)
-			fmt.Println(oc.Namespace())
 			project := oc.Namespace()
 
-			// TODO: Modify appropriate SCC (presumably restricted) to set FSGroup to "MustRunAs"
-			// This may not be necessary once this merges: https://github.com/openshift/origin/pull/7334
+			// TODO: Can be removed when PR linked above merges.
 			g.By("updated restricted SCC for fsGroup MustRunAs")
 			sccEditErr := setFSGroupMustRunAs(oc)
 			o.Expect(sccEditErr).NotTo(o.HaveOccurred())
 
-			g.By("make sure volume directory is on an XFS filesystem")
-			volDir := getEnvVar(volumeDirVar)
-			o.Expect(volDir).NotTo(o.Equal(""))
 			// Verify volDir is on XFS, if not this test can't pass:
-			// Use pre-existing utility in the empty_dir quota.go.
-			fmt.Printf("volDir = %s\n", volDir)
+			g.By("make sure volume directory is on an XFS filesystem")
+			volDir := os.Getenv(volDirEnvVar)
+			o.Expect(volDir).NotTo(o.Equal(""))
 			args := []string{"-f", "-c", "'%T'", volDir}
 			outBytes, _ := exec.Command("stat", args...).Output()
-			// If the volume directory is not on an XFS filesystem, this test cannot pass,
-			// so we'll fail it early.
 			o.Expect(strings.Contains(string(outBytes), "xfs")).To(o.BeTrue())
 
-			// Lookup the fsgroup for the pod's project. (first group ID in the supplemental range)
+			g.By("lookup test projects fsGroup ID")
 			fsGroup, err := lookupFSGroup(oc, project)
 			o.Expect(err).NotTo(o.HaveOccurred())
-			fmt.Printf("Found fsGroup for project: %d\n", fsGroup)
 
-			// TODO: Create a template that has an emptyDir volume, as simple as possible.
-			// Use hello-pod.json from examples?
-			g.By("create simple pod with emptyDir volume")
-			output, createPodErr := oc.Run("create").Args("-f", emptyDirPodFixture).Output()
+			g.By("create hello-openshift pod with emptyDir volume")
+			_, createPodErr := oc.Run("create").Args("-f", emptyDirPodFixture).Output()
 			o.Expect(createPodErr).NotTo(o.HaveOccurred())
-			fmt.Println(output)
 
-			// We need to wait for the pod to be created:
 			g.By("wait for XFS quota to be applied and verify")
-
-			// The quota takes a few seconds to appear, keep trying to find the correct value
-			// until we timeout. This allows for the possiblity a quota already exists for the
-			// gid from a past run, used for development purposes as you cannot easily delete a
-			// quota once applied.
-			secondsWaited := 0
-			quota := 0
-			for secondsWaited < podCreationTimeout {
-				quotaFound, quotaErr := lookupXFSQuota(oc, fsGroup, volDir)
-				o.Expect(quotaErr).NotTo(o.HaveOccurred())
-				if quotaFound == expectedQuotaKb {
-					quota = quotaFound
-					break
-				}
-
-				time.Sleep(1 * time.Second)
-				secondsWaited = secondsWaited + 1
-			}
-
-			// We applied 256Mi, xfs_quota reports in Kb:
-			fmt.Printf("Got quota: %d\n", quota)
-			o.Expect(quota).To(o.Equal(expectedQuotaKb))
+			lookupQuotaErr := waitForQuotaToBeApplied(oc, fsGroup, volDir)
+			o.Expect(lookupQuotaErr).NotTo(o.HaveOccurred())
 		})
 	})
 })
